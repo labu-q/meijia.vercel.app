d93518547080af492f39d2749c5d1041165f1342
labu-q <780060159@qq.com>
2026-08-23 19:54:20 +0800

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-export const seedData = {
-  customers: [],
-  technicians: [],
-  transactions: [],
-};
-
-export const incomeCategories = ['美甲服务', '手足护理', '储值充值', '产品销售', '其他收入'];
-export const expenseCategories = ['耗材采购', '房租水电', '工资提成', '营销推广', '设备工具', '其他支出'];
